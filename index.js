620d096e98b40e5cf2c8dd2e955c1c9beeb27661
Haeil Lim <lhi7837@gmail.com>
2021-03-29 15:21:04 +0900

    좆같은 JS

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const {User} = require("./models/User");
+
+
+app.use(bodyParser.urlencoded({exxtended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://lhi7837:lim9204@project.zcnes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log("MongoDB Connected!"))
 .catch(err => console.log(err))
 
-app.get('/',(req,res) => res.send('노드 서버'))
+app.get('/',(req,res) => res.send('노123드 서123버'))
+
+app.post('/register',(req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`해당 포트로 열림 : ${port}`))
